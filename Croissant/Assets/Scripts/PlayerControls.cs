c3ecef2f894775d043377e8f18882068253e6aec
JoyDeRuijter <joyderuijter@gmail.com>
2020-02-03 01:01:15 +0100

    Added basic scene sprites and basic scripts

---
+++ Croissant/Assets/Scripts/PlayerControls.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControls : MonoBehaviour
+{
+    public Rigidbody2D rb;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        rb.velocity = new Vector2(rb.velocity.x, -2);
+        if (Input.GetMouseButton(0))
+        {
+            rb.velocity = new Vector2(-4, rb.velocity.y);
+        }
+        else if (Input.GetMouseButton(1))
+        {
+            rb.velocity = new Vector2(4, rb.velocity.y);
+        }
+    }
+}
